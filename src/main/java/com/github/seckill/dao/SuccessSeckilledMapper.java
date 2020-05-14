037a7bce7467020b97ec45dcab14883cdb3405e4
bobwu <shunyuwu51@163.com>
2020-05-14 09:35:48 +0800

    增加SuccessSeckilledMapper

---
+++ src/main/java/com/github/seckill/dao/SuccessSeckilledMapper.java
@@ -0,0 +1,17 @@
+package com.github.seckill.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author bobwu
+ * @Title: SuccessSeckilledMapper
+ * @ProjectName seckill
+ * @date 2020-05-1409:34
+ */
+@Mapper
+public interface SuccessSeckilledMapper {
+
+
+
+
+}
