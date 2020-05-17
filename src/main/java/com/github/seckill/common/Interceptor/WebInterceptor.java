31a7862b63e05d2a7379dc2389b2a868cdc58a18
bobwu <shunyuwu51@163.com>
2020-05-17 20:51:37 +0800

    增加拦截器和过滤器的声明

---
+++ src/main/java/com/github/seckill/common/Interceptor/WebInterceptor.java
@@ -0,0 +1,15 @@
+package com.github.seckill.common.Interceptor;
+
+/**
+ * @author bobwu
+ * @Title: WebInterceptor
+ * @ProjectName seckill
+ * @date 2020-05-1720:50
+ */
+public class WebInterceptor {
+
+
+    //TODO
+    //拦截器实现 和  过滤器  实现功能
+    //https://juejin.im/post/5df4f5536fb9a0166243623c
+}
