7981686b63b0ec219331e2f35e12f2ddea9b74d7
wushunyu <shunyuwu51@163.com>
2020-05-19 21:21:56 +0800

    增加拦截器设置

--- src/main/java/com/github/seckill/common/Interceptor/WebInterceptor.java
+++ src/main/java/com/github/seckill/common/Interceptor/WebInterceptor.java
@@ -1,15 +1,37 @@
 package com.github.seckill.common.Interceptor;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author bobwu
  * @Title: WebInterceptor
  * @ProjectName seckill
  * @date 2020-05-1720:50
  */
-public class WebInterceptor {
-
-
-    //TODO
-    //拦截器实现 和  过滤器  实现功能
+public class WebInterceptor implements HandlerInterceptor {
     //https://juejin.im/post/5df4f5536fb9a0166243623c
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+//        request.getAttribute()
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+
+
 }
